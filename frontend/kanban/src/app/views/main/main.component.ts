622a6426c897d8264c0806bcaf303ba4c10bb266
Maryucha <mym@certi.org.br>
2022-03-17 23:46:32 -0300

    feature: rodando com a lista mocada

--- frontend/kanban/src/app/views/main/main.component.ts
+++ frontend/kanban/src/app/views/main/main.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,4 +13,21 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  todo = ['todo1', 'todo2', 'todo3'];
+  doing = ['doing1', 'doing2', 'doing3'];
+  done = ['done1', 'done2', 'done3'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
+
